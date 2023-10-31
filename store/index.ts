a19cd84f36a871a5861bbd46cd95949893bb678d
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-01 08:21:35 +0900

    error

--- store/index.ts
+++ store/index.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import postSlice from './module/post'
 import testSlice from './module/test'
 import deviceSlice from './module/device'
 import maskSlice from './module/mask'
 
 const store = configureStore({
   reducer: {
-    [postSlice.name]: postSlice.reducer,
     [deviceSlice.name]: deviceSlice.reducer,
     [maskSlice.name]: maskSlice.reducer,
     [testSlice.name]: testSlice.reducer
