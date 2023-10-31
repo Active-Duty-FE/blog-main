0d427c68defdc1ecd9471d8345f5c3948b97a52d
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-01 08:15:49 +0900

    --amend

--- store/module/post.ts
+++ store/module/post.ts
@@ -16,16 +16,3 @@ export const fetchPostById = createAsyncThunk('post', async (id: string) => {
   const res = await fetchPost(id)
   return res.data
 })
-const testSlice = createSlice({
-  name: 'post',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(fetchPostById.fulfilled, (state, action) => {
-      state.post = action.payload
-    })
-  }
-})
-
-export default testSlice
-export const {} = testSlice.actions
