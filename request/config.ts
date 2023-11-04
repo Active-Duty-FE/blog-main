5456e213250e0a9e21ec337f266c62342cae884d
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-04 11:44:13 +0900

    baseurl

--- request/config.ts
+++ request/config.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:3000/api'
+export const BASE_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'http://http://158.247.210.135:3000/api'
 export const TIMEOUT = 5000
