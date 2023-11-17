649963056a0f5e4f107cf7183c79b9cfae3e9689
hyanghee2020 <10122599+hyanghee2020@user.noreply.gitee.com>
2023-11-17 23:14:03 +0900

    change server to localhost

--- request/config.ts
+++ request/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://158.247.210.135:4000'
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://localhost:4000'
 export const TIMEOUT = 5001
